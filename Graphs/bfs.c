48a4934f97ae0643dc20e64cc60fa09b00669401
Srinivasulu Thiruveedula <31137003+tsrinu531@users.noreply.github.com>
2021-07-09 05:39:28 +0530

    Update bfs.c

--- Graphs/bfs.c
+++ Graphs/bfs.c
@@ -1,116 +1,55 @@
-// C program to implement Breadth first search
+// C program to implement BFS by using recursion
 #include<stdio.h>
-#include<stdlib.h>
- 
-#define MAX 20  
- 
-#define initial 1
-#define waiting 2
-#define visited 3
- 
-   
-int graph[MAX][MAX];
-int n; 
-int status[MAX];
-void create_graph();
-void BF_Traversal();
-void BFS(int v);
- 
-int queue[MAX], front = -1,rear = -1;
-void enqueue(int vertex);
-int dequeue();
-int isempty();
- 
+
+int graph[20][20],queue[20],visited[20],n,f=-1,r=-1;
+
+void bfs(int );
+
 void main()
 {
-	create_graph();
-	BF_Traversal();
+    int v,i,j;
+    printf("\n Enter the number of vertices:");
+    scanf("%d",&n);
+    for (i=1;i<=n;i++)    
+    {
+        visited[i]=0;
+    }
+    printf("\n Enter graph data in matrix form:\n");
+    for (i=1;i<=n;i++)
+      for (j=1;j<=n;j++)
+       scanf("%d",&graph[i][j]);
+       
+    printf("\n Enter the starting vertex:");
+    scanf("%d",&v);
+    f=r=0;
+    queue[r]=v;
+    
+	printf("\n BFS traversal is:\n");
+    visited[v]=1;   // mark the starting vertex as visited
+    printf("%d   ",v);
+   
+    bfs(v);
+    if(r != n-1)
+        printf("\n BFS is not possible");
 }
- 
-void BF_Traversal()
+
+void bfs(int v) 
 {
-	int v;
-	for(v=1; v<=n; v++)
-		status[v] = initial;
-	printf("Enter Start Vertex for BFS: \n");
-	scanf("%d", &v);
-	BFS(v);
-}
- 
-void BFS(int v)
-{
-	int i;
-	enqueue(v);
-	status[v] = waiting;
-	while(!isempty())
-	{
-		v = dequeue( );
-		printf("%d ",v);
-		status[v] = visited;
-		for(i=1; i<=n; i++)
-		{
-			if(graph[v][i] == 1 && status[i] == initial)
-			{
-				enqueue(i);
-				status[i] = waiting;
-			}
-		}
-	}
-	printf("\n");
-}
- 
-void enqueue(int vertex)
-{
-	if(rear == MAX-1)
-		printf("Queue Overflow\n");
-	else
-	{
-		if(front == -1)
-			front = 0;
-		rear = rear+1;
-		queue[rear] = vertex ;
-	}
-}
- 
-int isempty()
-{
-	if(front == -1 || front > rear)
-		return 1;
-	else
-		return 0;
-}
- 
-int dequeue()
-{
-	int delete_item;
-	if(front == -1 || front > rear)
-	{
-		printf("Queue Underflow\n");
-		exit(1);
-	}
-	delete_item = queue[front];
-	front = front+1;
-	return delete_item;
-}
- 
-void create_graph()
-{
-	int i,max_edge,row,column;
- 	printf("Enter number of vertices : ");
-	scanf("%d",&n);
-	max_edge = n*(n-1);
- 	for(i=1; i<=max_edge; i++)
-	{
-		printf("Enter edge %d( 0 0 to quit ) : ",i);
-		scanf("%d %d",&row,&column);
- 		if((row == -1) && (column == -1))
-			break;
-		if(row>n || column>n || row<=0 || column<=0)
-		{
-			printf("Invalid edge!\n");
-			i--;
-		}
-		else
-			graph[row][column] = 1;
-	}
+       int i;
+       // check all the vertices in the graph
+       for (i=1;i<=n;i++)                                
+       {
+       	
+       	// adjacent to v and not visited
+               if(graph[v][i] != 0 && visited[i] == 0) 
+              {
+                       r=r+1;
+                       queue[r]=i;      // insert them into queue
+                       visited[i]=1;    // mark the vertex visited
+                       printf("%d  ",i);
+              }
+      }
+      f=f+1;      // remove the vertex at front of the queue
+      if(f<=r)    // as long as there are elements in the queue
+            bfs(q[f]);  // peform bfs again on the vertex at front of the queue
 }
